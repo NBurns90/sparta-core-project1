af0d134515a42b50c67f95fdf444c8e011c2ef86
NBurns90 <natburns@gmail.com>
2018-02-05 13:44:00 +0000

    first commit - base boilerplates and file structure

---
+++ javascript/app.js
@@ -0,0 +1,46 @@
+console.log('This Page is Linked');
+
+var puzzleBoard = $('puzzleBoard');
+
+// Pseudocode
+//
+//
+// Define the variable to store the puzzleBoard element
+// Append the puzzleBoard with each element (3x3 for now)
+//
+//
+//
+
+
+//
+// for (var r = 0; r < 3; i++) {
+//   var rows = $('div');
+//   rows.setAttribute('class', 'row');
+//   puzzleBoard.appendChild('row');
+//
+//   for (var c = 0; c < 3; i++) {
+//     var cols = $('div');
+//     cols.setAttribute('class', 'col');
+//     rows.appendChild('col');
+//   }
+// }
+
+
+
+
+
+
+
+
+
+// for (var i = 0; i < 5; i++) {
+//   var rows = document.createElement('div');
+//   rows.setAttribute('class', 'row');
+//   myBoard.appendChild(rows);
+//
+//   for (var j = 0; j < 5; j++) {
+//     var cols = document.createElement('div');
+//     cols.setAttribute('class', 'col');
+//     rows.appendChild(cols);
+//   }
+// }
